ea6981f585c89f6d52303f14f72015de767aaf8b
GreenRain <47217640+greenrain78@users.noreply.github.com>
2023-11-07 16:30:04 +0900

    feat(LottoPrizes): 로또 당첨금 객체 구성
    - 등수별 금액을 책정

---
+++ src/main/kotlin/lotto/constant/LottoPrizes.kt
@@ -0,0 +1,10 @@
+package lotto.constant
+
+enum class LottoPrizes(val value: Int) {
+    FIRST_PRIZE(2000000000),
+    SECOND_PRIZE(30000000),
+    THIRD_PRIZE(1500000),
+    FOURTH_PRIZE(50000),
+    FIFTH_PRIZE(5000),
+    NONE_PRIZE(0),
+}
