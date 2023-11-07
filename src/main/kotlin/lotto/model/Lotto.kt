d5b48673671bde2c1fa890b794819e80703be21f
GreenRain <47217640+greenrain78@users.noreply.github.com>
2023-11-07 16:32:17 +0900

    feat(LottoChecker): 로또 당첨금을 확인하는 객체 구성
    - checkLottoList: 로또들을 확인해서 각 등수별 개수를 반환
    - checkLottoOne: 로또 하나가 당첨되었는지 확인

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,20 +4,31 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == NUMBER_SIZE)
     }
 
-    fun getNumbers(): List<Int> {
-        return numbers.sorted()
+    internal fun getNumbers(): List<Int> = numbers
+    fun getNumbers(isAsc: Boolean) = when (isAsc) {
+        true -> numbers.sorted()
+        false -> numbers.reversed()
+    }
+
+    override fun toString(): String {
+        return "$numbers"
     }
 
     companion object {
+        const val PRICE = 1000
         private const val MIN_NUM = 1
         private const val MAX_NUM = 45
-        private const val NUMBER_SIZE = 6
+        const val NUMBER_SIZE = 6
         fun createRandom(): Lotto {
             val numbers = Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, NUMBER_SIZE)
             return Lotto(numbers)
+        }
+
+        fun createNumList(numList: List<Int>): Lotto {
+            return Lotto(numList)
         }
     }
 }
---
+++ src/main/kotlin/lotto/model/LottoChecker.kt
@@ -0,0 +1,33 @@
+package lotto.model
+
+import lotto.constant.LottoPrizes
+
+class LottoChecker(private val jackpotNumbers: List<Int>, private val bonusNum: Int) {
+    fun checkLottoList(lottoList: List<Lotto>): Map<LottoPrizes, Int> {
+        val prizeList = lottoList.map { lotto: Lotto ->
+            checkLottoOne(lotto.getNumbers())
+        }
+        val prizeCounts = prizeList.groupingBy { it }.eachCount()
+        println(prizeList)
+        println(prizeCounts)
+        println(LottoPrizes.entries.associateWith { prizeCounts.getOrDefault(it, 0) })
+        return LottoPrizes.entries.associateWith { prizeCounts.getOrDefault(it, 0) }
+
+    }
+
+    private fun checkLottoOne(lottoNumbers: List<Int>): LottoPrizes {
+        val correctCount = lottoNumbers.intersect(jackpotNumbers.toSet()).size
+        return when (correctCount) {
+            6 -> LottoPrizes.FIRST_PRIZE
+            5 -> when (lottoNumbers.contains(bonusNum)) {
+                true -> LottoPrizes.SECOND_PRIZE
+                false -> LottoPrizes.THIRD_PRIZE
+            }
+
+            4 -> LottoPrizes.FOURTH_PRIZE
+            3 -> LottoPrizes.FIFTH_PRIZE
+            else -> LottoPrizes.NONE_PRIZE
+        }
+
+    }
+}
